9aa4f7ebcee60a5c18da698d2737cf19ccf410a3
Schenier Lopez <schenier.lopez@powerplanetonline.com>
2020-04-30 15:25:39 +0200

    Step TargetDate

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -1,18 +1,27 @@
 import React from 'react'
 
-interface CountdownProps {}
+interface CountdownProps {
+    targetDate: string
+}
 
-const Countdown: StorefrontFunctionComponent<CountdownProps> = ({}) => {
-  return <div>
-    <h1>Oi teste</h1>
-  </div>
+const Countdown: StorefrontFunctionComponent<CountdownProps> = ({targetDate}) => {
+    return <div>
+        <h1>{targetDate}</h1>
+    </div>
 }
 
 Countdown.schema = {
-  title: 'editor.countdown.title',
-  description: 'editor.countdown.description',
-  type: 'object',
-  properties: {},
+    title: 'editor.countdown.title',
+    description: 'editor.countdown.description',
+    type: 'object',
+    properties: {
+        targetDate: {
+            title: 'Final date',
+            description: 'Final date used in the countdown',
+            type: 'string',
+            default: null
+        }
+    },
 }
 
 export default Countdown
